68291840652904e9a7f5cc3b6572c3ed7c96296d
Chunhoyoung <ho723@naver.com>
2021-03-21 03:19:32 +0900

    피드백반영

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,9 +7,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    #객체의 표현을 보기쉽게 하기 위해 추가한 함수
     def __str__(self):
         return self.question_text
 
+    #최근에 발행됐는지 확인해주는 함수
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,8 +4,39 @@ from django.urls import reverse
 from django.views import generic
 from .models import Choice, Question
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
 
-class IndexView(generic.ListView):
+#시스템에 저장된 최소한 5 개의 투표 질문이 콤마로 분리되어, 발행일에 따라 출력됩니다.
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+#polls/index.html 템플릿을 불러온 후, context를 전달합니다.
+#context는 템플릿에서 쓰이는 변수명과 Python 객체를 연결하는 사전형 값입니다.
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+#
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+#
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
+
+#제네릭뷰 사용
+class IndexView(generic.ListView): #Question 리스트 보여준다
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
@@ -14,17 +45,17 @@ class IndexView(generic.ListView):
         return Question.objects.order_by('-pub_date')[:5]
 
 
-class DetailView(generic.DetailView):
+class DetailView(generic.DetailView): #Question을 보여준다
     model = Question
     template_name = 'polls/detail.html'
 
 
-class ResultsView(generic.DetailView):
+class ResultsView(generic.DetailView): #Vote결과를 보여준다
     model = Question
     template_name = 'polls/results.html'
 
 
-def vote(request, question_id):
+def vote(request, question_id): #Vote하는 페이지
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
